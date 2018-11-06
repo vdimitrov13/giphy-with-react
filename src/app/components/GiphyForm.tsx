b8dd9a3826f4472656f39b6d4577dfedf92354f8
Vladimir Dimitrov <vladimir.d@sbtech.com>
2018-11-06 14:35:07 +0200

    removed unnecessary interface declaration

--- src/app/components/GiphyForm.tsx
+++ src/app/components/GiphyForm.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
-import { observer } from 'mobx-react';
 
-interface CustomInputProps{
+export class GiphyForm extends React.Component< {
     handleSubmit: (event) => void;
     handleChange: (event) => void;
     formState: string;
-}
+}, {} > { 
 
-@observer
-export class GiphyForm extends React.Component<CustomInputProps> { 
     render() {
         return (
         <form onSubmit={this.props.handleSubmit}>
             <label htmlFor="GIF">Search GIF</label>
             <input id="GIF" name="GIF" type="text" value ={this.props.formState} onChange={this.props.handleChange}/>     
-            <button>Send data!</button>
+            <button>Find GIF</button>
         </form>
         );
     }
